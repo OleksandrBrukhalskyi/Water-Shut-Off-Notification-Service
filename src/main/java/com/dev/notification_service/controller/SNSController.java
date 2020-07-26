0178ab49b35fdf8e29354a3180baef88fd44b86c
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-07-26 19:13:13 +0300

    feat: implement publish message to topic.

--- src/main/java/com/dev/notification_service/controller/SNSController.java
+++ src/main/java/com/dev/notification_service/controller/SNSController.java
@@ -2,22 +2,19 @@ package com.dev.notification_service.controller;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sns.AmazonSNSClient;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sns.model.CreateTopicResult;
 import com.amazonaws.services.sns.model.ListTopicsResult;
+import com.amazonaws.services.sns.model.PublishResult;
 import com.amazonaws.services.sns.model.Topic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-import software.amazon.awssdk.services.sns.model.ListTopicsRequest;
-import software.amazon.awssdk.services.sns.model.ListTopicsResponse;
-import software.amazon.awssdk.services.sns.model.SnsException;
-import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
 import software.amazon.awssdk.services.sns.SnsClient;
-import software.amazon.awssdk.services.sns.model.CreateTopicRequest;
-import software.amazon.awssdk.services.sns.model.CreateTopicResponse;
 import software.amazon.awssdk.services.sns.model.SnsException;
 
 import java.util.List;
@@ -34,6 +31,7 @@ public class SNSController {
 
 
 
+
     @GetMapping("/api/v1/topics")
     public List<Topic> getTopics(){
         ListTopicsResult listTopicsResult = snsClient.listTopics();
@@ -47,19 +45,17 @@ public class SNSController {
       return topics;
     }
     @PostMapping("/api/v1/createTopic/")
-    public String createTopic(@RequestParam("topicName") String topicName
-                              ){
+    public String createTopic(@RequestParam("topicName") String topicName){
+        CreateTopicResult result = snsClient.createTopic(topicName);
+            return "Топік створено успішно!" + result.getTopicArn();
+    }
+    @PostMapping("/api/v1/pubToTopic")
+    public String publishToTopic(@RequestParam("message") String message,
+                                 @RequestParam("topicArn") String topicArn,
+                                 @RequestParam("subject") String subject
+                            ){
+      snsClient.publish(topicArn,message,subject);
+      return "Message sent to " + topicArn;
 
-        try{
-            CreateTopicRequest request = CreateTopicRequest.builder()
-                    .name(topicName)
-                    .build();
-            CreateTopicResult result = snsClient.createTopic(String.valueOf(request));
-            return result.getTopicArn();
-        }
-        catch (SnsException e){
-            System.err.print(e.awsErrorDetails().errorMessage());
-        }
-        return "Топік успіншно створено!";
     }
 }
